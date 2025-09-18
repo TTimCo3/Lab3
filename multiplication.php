ec383c0a0f511ea5b84c71e4b5240cb5f57c4f5d
Trey Timko <t342t073@ku.edu>
2025-09-18 13:33:09 -0500

    Modifications to both the php file for the multiplication table and the practice4 html file

--- multiplication.php
+++ multiplication.php
@@ -1,17 +1,25 @@
 <html>
 <body>
-
+<pre>
 <?php 
-$size=$_GET["size"];
-<br>
-for($i=1;$i<$size;$i++){
-    for($j=1;$j<$size;$j++){
-        echo " " . $i*$j   
-    }
-    echo "\n"
-}
+	$size=$_POST["size"];
+	echo " ";
+	for($i=1;$i<=$size;$i++){
+		echo str_pad("    " . $i, 5, " ");
+	}
+	for($i=1;$i<=$size;$i++){
+		echo "\n";
+		for($j=0;$j<=$size;$j++){
+			if($j==0){
+				echo str_pad($i . " ",5," ");
+			}
+			else{
+				echo str_pad($j*$i . " ",5," ");
+			}
+		}
+	}
 ?>
-
+</pre>
 
 </body>
 </html>
